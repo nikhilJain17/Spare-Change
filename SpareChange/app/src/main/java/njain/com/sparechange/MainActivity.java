b8094a47d1bdacefc1374b06c9e9ed10ecec1481
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-09-24 19:42:31 -0400

    open cv's camera view display works

--- SpareChange/app/src/main/java/njain/com/sparechange/MainActivity.java
+++ SpareChange/app/src/main/java/njain/com/sparechange/MainActivity.java
@@ -2,12 +2,89 @@ package njain.com.sparechange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.TextureView;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private JavaCameraView mCameraView; // handler for the camera
+    private OpenCVLoader mOpenCVLoader; // load opencv boi
+
+    // Listener anonymous class, handles callbacks
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.d("MainActivity", "Loaded OpenCV successfully!");
+                    mCameraView.enableView();
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                }
+            } // end of switch
+
+        } // end of onManagerConnected
+
+        @Override
+        public void onPackageInstall(int operation, InstallCallbackInterface callback) {
+            super.onPackageInstall(operation, callback);
+        } // end of onPackageInstall
+
+    }; // end of mLoaderCallback
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraView = (JavaCameraView) findViewById(R.id.CameraView);
+        mCameraView.setVisibility(SurfaceView.VISIBLE);
+        mCameraView.setCvCameraViewListener(this);
+
+    } // end of onCreate
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback); // init opencv
+
+    } // end of onResume
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mCameraView != null)
+            mCameraView.disableView();
     }
-}
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override // when camera delivers a frame
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+} // end of MainActivity
