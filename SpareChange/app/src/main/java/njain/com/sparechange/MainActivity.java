7362aaedeef40c44132c15a5a95bc721ab0b21a2
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-09-24 20:38:49 -0400

    fullscreen and rotation free boys

--- SpareChange/app/src/main/java/njain/com/sparechange/MainActivity.java
+++ SpareChange/app/src/main/java/njain/com/sparechange/MainActivity.java
@@ -1,10 +1,16 @@
 package njain.com.sparechange;
 
+import android.app.ActionBar;
+import android.app.Notification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.TextureView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -14,7 +20,7 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener {
 
     private JavaCameraView mCameraView; // handler for the camera
     private OpenCVLoader mOpenCVLoader; // load opencv boi
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
-                    Log.d("MainActivity", "Loaded OpenCV successfully!");
+                    Log.i("MainActivity", "Loaded OpenCV successfully!");
                     mCameraView.enableView();
                     break;
                 }
@@ -52,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mCameraView = (JavaCameraView) findViewById(R.id.CameraView);
         mCameraView.setVisibility(SurfaceView.VISIBLE);
         mCameraView.setCvCameraViewListener(this);
@@ -62,6 +69,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     protected void onResume() {
         super.onResume();
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback); // init opencv
+
+        // hide the cheeky actionbar
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
     } // end of onResume
 
@@ -75,16 +88,34 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        // Mat tempMat = new Mat();
+        // tempMat.release() <-- java has auto garbage collection but c does not
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        // tempMat.release()
     }
 
     @Override // when camera delivers a frame
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        // DO NOT INSTANTIATE MAT'S HERE! This is called everytime the camera delivers a frame.
+
         return inputFrame.rgba();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        Log.i("MainActivity", "Touched");
+
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                Toast.makeText(this, "Analyzing", Toast.LENGTH_SHORT).show();
+        } // end of switch
+
+        return true;
     }
 } // end of MainActivity
