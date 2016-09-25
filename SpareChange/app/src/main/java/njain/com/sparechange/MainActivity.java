37ae117423929dac92d45faac185f1605aed029b
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-09-24 22:58:42 -0400

    detects circle radius and position!

--- SpareChange/app/src/main/java/njain/com/sparechange/MainActivity.java
+++ SpareChange/app/src/main/java/njain/com/sparechange/MainActivity.java
@@ -18,8 +18,13 @@ import org.opencv.android.InstallCallbackInterface;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener {
 
@@ -107,9 +112,42 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         // DO NOT INSTANTIATE MAT'S HERE! This is called everytime the camera delivers a frame.
 
         tempMat = inputFrame.gray();
+        Mat circles = new Mat();
 
         // edge detection!
-        Imgproc.Canny(tempMat, tempMat, 50, 100);
+        Imgproc.Canny(tempMat, tempMat, 100, 100);
+        Imgproc.HoughCircles(tempMat, circles, Imgproc.HOUGH_GRADIENT, 69, 69);
+
+        int iCannyUpperThreshold = 100;
+        int iMinRadius = 20;
+        int iMaxRadius = 400;
+        int iAccumulator = 300;
+
+        Imgproc.HoughCircles(tempMat, circles, Imgproc.CV_HOUGH_GRADIENT,
+                2.0, tempMat.rows() / 8, iCannyUpperThreshold, iAccumulator,
+                iMinRadius, iMaxRadius);
+
+        if (circles.cols() > 0)
+            for (int x = 0; x < circles.cols(); x++)
+            {
+                double vCircle[] = circles.get(0,x);
+
+                if (vCircle == null)
+                    break;
+
+                Point pt = new Point(Math.round(vCircle[0]), Math.round(vCircle[1]));
+                int radius = (int)Math.round(vCircle[2]);
+                Log.i("PointyDick", "xy" + pt.toString());
+                Log.i("PointyDick", "radius" + Integer.toString(radius));
+                // draw the found circle
+                Imgproc.circle(tempMat, pt, radius, new Scalar(0,255,0), 10);
+                Imgproc.circle(tempMat, pt, 3, new Scalar(0, 0, 255), 10);
+            }
+
+//        if (circleMat != null)
+//            Imgproc.circle(tempMat, new Point(150, 150), 69, new Scalar(100, 100, 100));
+
+//        Imgproc.circle(tempMat, , , Imgproc.COLOR_BGR2BGR555);
 //        Imgproc.dilate(tempMat, tempMat, tempMat)
 
 
